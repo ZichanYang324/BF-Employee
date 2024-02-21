13450ee36eda05d620b9f7e1609ed54d6d757de5
christorange <christol.yu@outlook.com>
2024-02-20 21:40:41 -0500

    init: server

---
+++ Server/index.js
@@ -0,0 +1,15 @@
+import app from './app.js';
+import process from 'process';
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(3100, () => {
+    console.log('Server listening on port 3100', 'http://localhost:3100');
+  });
+}).catch((err) => {
+  console.log('Failed to connect to MongoDB', err);
+})
+
