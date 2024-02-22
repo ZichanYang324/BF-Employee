5e74782d2e0de7436d6ac9b41b1329c9451b90d4
Yue Yu <yue.yu.career@gmail.com>
2024-02-22 13:53:06 -0800

    example read s3 file

--- Server/app.js
+++ Server/app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import path from "path";
 import process from "process";
 import cookieParser from "cookie-parser";
+import DevRouter from "./routers/DevRouter.js";
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use("/public", express.static(path.join(process.cwd(), "public")));
 app.get("/", (_req, res) => {
   res.status(200).send("<h1>Server started</h1>");
 });
+
+app.use("/dev", DevRouter);
 
 app.all("*", (_req, res) => {
   res.status(404).send("<h1>Page not found</h1>");
--- Server/routers/DevRouter.js
+++ Server/routers/DevRouter.js
@@ -5,12 +5,21 @@ import { getOneFile } from "../utils/s3.js";
 
 const router = Router();
 
-router.get("read", async (_req, res) => {
-  const file = await getOneFile({ Key: "hello.txt" });
-  if (file.error) {
+const streamToString = async (stream) => {
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
+};
+
+router.get("/read", async (_req, res) => {
+  const { data: file, error } = await getOneFile({ Key: "hello.txt" });
+  if (error) {
     return res.status(500).send("<h1>Internal server error</h1>");
   }
-  res.status(200).send(file.data?.Body?.toString());
+  const fileContent = await streamToString(file.Body);
+  return res.status(200).send(fileContent);
 });
 
 export default router;
