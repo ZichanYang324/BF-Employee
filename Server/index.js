d4b05a98791ee24dadf49aff53fbaebfafdbd265
Yue Yu <yue.yu.career@gmail.com>
2024-02-22 13:21:00 -0800

    add prettier, formatted code

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,16 @@
-import app from './app.js';
-import process from 'process';
+import app from "./app.js";
+import process from "process";
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(3100, () => {
-    console.log('Server listening on port 3100', 'http://localhost:3100');
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3100, () => {
+      console.log("Server listening on port 3100", "http://localhost:3100");
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
   });
-}).catch((err) => {
-  console.log('Failed to connect to MongoDB', err);
-})
-
