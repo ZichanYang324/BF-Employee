9a49156f438664ea3af9b55f9deca3ad684c712a
Xiangwei Chen <xiangwei@teave.co>
2024-02-23 14:33:26 -0500

    added housing, facility, comment model to the index.js, and formatted files for PR

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,5 +1,8 @@
 import User from "./User.model.js";
 import Profile from "./Profile.model.js";
 import Document from "./Document.model.js";
+import Housing from "./Housing.model.js";
+import FacilityReport from "./FacilityReport.model.js";
+import Comment from "./Comment.model.js";
 
-export { User, Profile, Document };
+export { User, Profile, Document, Housing, FacilityReport, Comment };
