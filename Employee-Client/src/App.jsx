edd711cfbb31fdd0fd2c6a0fee989b20b0e38aba
ZichanYang324 <yzc97324@gmail.com>
2024-02-24 14:40:30 -0500

    add navbar

--- Employee-Client/src/App.jsx
+++ Employee-Client/src/App.jsx
@@ -1,5 +1,14 @@
+import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 function App() {
-  return <></>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="" element={<NotFound />} />
+  </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
