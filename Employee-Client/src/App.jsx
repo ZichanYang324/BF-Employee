5b289e7aba93cf317236b9dbb549947b1bc61815
ZichanYang324 <yzc97324@gmail.com>
2024-02-25 00:49:28 -0500

    add register page

--- Employee-Client/src/App.jsx
+++ Employee-Client/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./components/login";
+import { Register } from "./components/register";
+import Dashboard from "./pages/dashboard";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>)
 }
 
 export default App;
