75017b4e65a4999e279de195153e60a05e7edc87
Xiangwei Chen <xiangwei@teave.co>
2024-02-28 00:34:00 -0500

    added basic housing info

--- Employee-Client/src/App.jsx
+++ Employee-Client/src/App.jsx
@@ -1,20 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Housing } from "./components/housing";
 import { Login } from "./components/login";
 import { Register } from "./components/register";
-import { Housing } from "./components/housing";
 import Dashboard from "./pages/dashboard";
 import ProtectedRoute from "./pages/protectedRoute";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="register" element={<Register />} />
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path="housing" element={<Housing />} />
+        <Route
+          path="housing"
+          element={
+            <ProtectedRoute>
+              <Housing />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
-    </BrowserRouter>)
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- Employee-Client/src/components/housing/index.jsx
+++ Employee-Client/src/components/housing/index.jsx
@@ -1,11 +1,69 @@
 import ResponsiveAppBar from "../navbar";
-import React from "react";
+import "./style.css";
+import GroupIcon from "@mui/icons-material/Group";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
 export const Housing = () => {
   return (
     <>
       <ResponsiveAppBar />
-      <div>Housing</div>
+
+      <Box sx={{ padding: "2rem" }}>
+        <Typography variant="h5">Housing Infomation</Typography>
+      </Box>
+
+      <Box className="cardContainer">
+        <Card sx={{ width: "80%", height: "14rem" }} variant="outlined">
+          <CardContent>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"baseline"}
+            >
+              <Typography sx={{ fontSize: 20 }} gutterBottom>
+                110 warn street
+              </Typography>
+              <Box sx={{ display: "flex" }}>
+                <CardActions>
+                  <Button
+                    size="small"
+                    variant="contained"
+                    color="warning"
+                    disabled
+                  >
+                    View Report
+                  </Button>
+                </CardActions>
+
+                <CardActions>
+                  <Button size="small" variant="contained" color="error">
+                    New Report
+                  </Button>
+                </CardActions>
+              </Box>
+            </Box>
+            <GroupIcon sx={{ fontSize: 40, mt: 2 }} />
+          </CardContent>
+          <Box padding={"0px 10%"} display={"flex"}>
+            <Box marginRight={"10px"}>
+              <Typography sx={{ fontSize: 15 }}>James-7188380000 </Typography>
+            </Box>{" "}
+            <Box marginRight={"10px"}>
+              <Typography sx={{ fontSize: 15 }}>James-7188380000 </Typography>
+            </Box>{" "}
+            <Box marginRight={"10px"}>
+              <Typography sx={{ fontSize: 15 }}>James-7188380000 </Typography>
+            </Box>
+          </Box>
+        </Card>
+      </Box>
     </>
   );
 };
