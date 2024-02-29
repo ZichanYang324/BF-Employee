4d44f54f4bd82aefcb51a1a23a3a42f396aa9757
Yue Yu <yue.yu.career@gmail.com>
2024-02-29 15:33:38 -0800

    display profile summary

--- HR-Client/src/app/app.module.ts
+++ HR-Client/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { EmployeeProfilesComponent } from './pages/employee-profiles/employee-profiles.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EmployeeProfilesComponent } from './pages/employee-profiles/employee-profiles.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavBarComponent, EmployeeProfilesComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    EmployeeProfilesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
+    MatListModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- HR-Client/src/app/pages/employee-profiles/employee-profiles.component.ts
+++ HR-Client/src/app/pages/employee-profiles/employee-profiles.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+const TEST_SUMMARY = {
+  firstName: 'Alice',
+  middleName: 'B',
+  lastName: 'Cooper',
+  SSN: '123-45-6789',
+  workAuthTitle: 'OPT',
+  phoneNumber: '123-456-7893',
+  email: 'user1@mail.com',
+};
+interface ProfileSummary {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  SSN: string;
+  workAuthTitle: string;
+  phoneNumber: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-employee-profiles',
   templateUrl: './employee-profiles.component.html',
-  styleUrls: ['./employee-profiles.component.css']
+  styleUrls: ['./employee-profiles.component.css'],
 })
 export class EmployeeProfilesComponent implements OnInit {
+  profileSummaries: ProfileSummary[] = [TEST_SUMMARY];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
