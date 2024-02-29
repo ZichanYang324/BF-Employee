1a6ade95da5d5a48c255b06d2294a8f2aae29ca6
Yue Yu <yue.yu.career@gmail.com>
2024-02-29 15:08:19 -0800

    add hr profiles page

--- HR-Client/src/app/app-routing.module.ts
+++ HR-Client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EmployeeProfilesComponent } from './pages/employee-profiles/employee-profiles.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'employee-profiles',
+    component: EmployeeProfilesComponent,
   },
 ];
 
--- HR-Client/src/app/app.module.ts
+++ HR-Client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeProfilesComponent } from './pages/employee-profiles/employee-profiles.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavBarComponent],
+  declarations: [AppComponent, HomeComponent, NavBarComponent, EmployeeProfilesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
