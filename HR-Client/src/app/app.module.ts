1d9f2663a29e53234cd638534e880c38b8addd27
christorange <christol.yu@outlook.com>
2024-02-28 23:42:11 -0500

    feat: generate home page and nav bar files

--- HR-Client/src/app/app.module.ts
+++ HR-Client/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, NavBarComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
